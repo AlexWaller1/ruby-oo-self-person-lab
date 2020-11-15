0cee1838b0f9b9f919f4acbe8a0948477967b859
Alex Waller <alex@artofalexwaller.com>
2020-11-14 21:39:36 -0500

    commit tree 5a04857c9811e9f5826dd8c1c3e7ca720116b253

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,46 @@
 # your code goes here
+class Person
+attr_accessor :name
+    def initialize(name)
+        @name = name
+end
+
+def clean?
+
+end
+
+def happy?
+
+end
+
+def get_paid(salary_amount)
+
+    @salary_amount
+
+end
+
+def take_bath
+
+end
+
+def work_out
+
+end
+
+def call_friend(friend)
+    @friend
+
+end
+
+def start_conversation(person, topic)
+
+    @topic = topic
+
+end
+
+def hygiene
+    
+
+end
+
+end
